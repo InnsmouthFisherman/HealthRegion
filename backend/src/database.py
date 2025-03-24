358720bac4336ac142543d4c4f5694a723bf88e2
InnsmouthFisherman <76620415+InnsmouthFisherman@users.noreply.github.com>
2025-03-24 10:37:33 +0500

    table_loading

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
+import pandas as pd
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -11,3 +12,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def load_table(path):    
+    df = pd.read_csv(path)
+    
+    df.to_sql("test_table", con=engine, if_exists="replace", index=False)
+
+    print(f"Таблица 'test_table' успешно загружена в базу данных.")
